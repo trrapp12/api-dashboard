96a21bbd6ca4d238244748c074a50ad3ac1a4268
Trevor Rapp <11747875+trrapp12@users.noreply.github.com>
2023-10-04 19:53:32 -0600

    stuff

--- js/timer.js
+++ js/timer.js
@@ -5,12 +5,25 @@
     const cubeContainer = document.getElementById('scene');
     const pomodorroContainer = document.getElementById('pomodorro')
     const onePomodorro = 25 * 60 * 1000;
+    const playButton = document.getElementById('play');
+    const pauseButton = document.getElementById('pause')
     let endTime;
+    let pomodorroPlaying = false;
 
     cubeContainer.addEventListener("click", (evt) => {
-        if(evt.target.closest('#scene')) {
+        if (evt.target.id === 'play') {
+            pomodorroPlaying = true
+            playButton.classList.toggle('hide');
+            pauseButton.classList.toggle('hide')
+        } else if (evt.target.id === 'pause') {
+            pomodorroPlaying = true
+            playButton.classList.toggle('hide');
+            pauseButton.classList.toggle('hide')
+        } else if (evt.target.closest('#scene')) {
             console.log('flipping card')
             document.getElementById('cube').classList.toggle('is-flipped')
+        } else {
+            console.log('go away')
         }
     })
 
@@ -31,7 +44,7 @@
             let minutes = Math.floor(timeRemaining / 60000);
             let seconds = Math.floor((timeRemaining % 60000) / 1000);
             console.log(seconds)
-            pomodorroContainer.textContent = (`${minutes}${seconds < 10 ? '0' : ''}:${seconds}`);
+            pomodorroContainer.textContent = (`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`);
         }
 
     }

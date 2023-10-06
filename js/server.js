e3209b64d6261f51c9e374a25f42b93389b29be6
Trevor Rapp <11747875+trrapp12@users.noreply.github.com>
2023-10-05 22:43:54 -0600

    fixes css issues, displays author's name, begins work for weather api

--- js/server.js
+++ js/server.js
@@ -9,9 +9,7 @@ app.use(cors());
 
 const port = process.env.PORT || 5501;
 
-// const agent = new https.Agent({  
-//     rejectUnauthorized: false
-// });
+console.log(Navigator.geolocation)
 
 app.get('/getBackground/', async (req, res) => {
     // unsplash api
@@ -44,38 +42,20 @@ app.get('/getBackground/', async (req, res) => {
 // });
 
 
-// app.get('/getWeather/:lat/:lon', async (req, res) => {
-//     // openweathermap api
-//     try {
-//         const response = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
-//             params: {
-//                 lat: req.params.lat,
-//                 lon: req.params.lon,
-//                 appid: process.env.OPENWEATHERMAP_ACCESS_KEY,
-//                 units: 'metric'
-//             }
-//         })
-//         res.send(response.data);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send(error);
-//     }
-// });
-
-app.get('/getWord', async (req, res) => {
-    // OED api
+app.get('/getWeather/:lat/:lon', async (req, res) => {
+    // openweathermap api
     try {
-        const response = await axios.get('https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/ace', {
-            headers: {
-                'app_id': process.env.OED_APP_ID,
-                'app_key': process.env.OED_APP_KEY
+        const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${minutely,hourly,daily}`, {
+            params: {
+                lat: req.params.lat,
+                lon: req.params.lon,
+                appid: process.env.OPEN_WEATHER_API_KEY,
+                units: 'standard'
             }
         })
         res.send(response.data);
     } catch (error) {
         console.error(error);
-        res.status(500).send(error)
+        res.status(500).send(error);
     }
 });
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
